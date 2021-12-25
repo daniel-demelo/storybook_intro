060891e2b2e36bf5f0435f07a415cf7dafc7a2c0
Daniel Melo <emaildanielmelo@gmail.com>
2021-12-24 21:33:59 -0300

    Add simple component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,8 @@
 module.exports = {
-  stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx|svelte)",
-  ],
+  stories: ['../src/components/**/*.stories.js'],
   staticDirs: ["../public"],
   addons: [
+    '@storybook/addon-actions',
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-svelte-csf",
